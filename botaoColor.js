1781ab20d55f1d79eea2f48f983849947e1ff87b
lucslucena <31022780+lucslucena@users.noreply.github.com>
2020-02-10 11:54:06 -0300

    primeiro commit js

---
+++ botaoColor.js
@@ -0,0 +1,6 @@
+document.getElementById('green').addEventListener('click', function(){
+        document.getElementById('quadrado').style.backgroundColor = '#11b819';
+});
+   
+document.getElementById('red').addEventListener('click', function(){
+       document.getElementById('quadrado').style.backgroundColor = '#f00'
