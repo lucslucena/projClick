fef566eb43ee194cf85754a8ba35e4a361fffd48
lucslucena <31022780+lucslucena@users.noreply.github.com>
2020-02-11 08:21:13 -0300

    update script.js

--- desafio-script.js
+++ desafio-script.js
@@ -1,4 +1,54 @@
+/*5 
 document.getElementById('btn-exibir-nome').addEventListener("click", function() {
-            const nomeC = document.getElementById('nomeC').value;
-            alert("O nome completo é " + nomeC);     
+            const name = document.getElementById('nomeC').value;
+            alert("O nome completo é " + name.length);     
         });
+*/
+
+/* 6
+test = document.getElementById('acender').addEventListener("click", function(){
+   this.src = "C:\\Users\\LESTANISLAU\\Desktop\\projSexta\\lampada-on.jpg";
+});
+*/
+
+/* 7
+test = document.getElementById('passar').addEventListener('mouseover', function(){
+   this.src = "C:\\Users\\LESTANISLAU\\Desktop\\projSexta\\lampada-on.jpg";
+});
+
+*/
+
+/* 8
+document.getElementById('ex-nome').addEventListener('click', function(){
+    const nomeP = document.getElementById('nome').value;
+    const sobrename = document.getElementById('sobrenome').value;
+    alert("Olá, " + nomeP + ' ' + sobrename);                                            
+});
+*/
+
+/* 10
+document.getElementById('valor-soma').addEventListener('click', function(){
+    const n1 = parseInt(document.getElementById("num1").value);
+    const n2 = parseInt(document.getElementById("num2").value);
+    const soma = (n1 + n2);
+    document.getElementById('resultado').value=soma;
+});
+*/
+
+
+/* 12
+document.getElementById('green').addEventListener('click', function(){
+        document.getElementById('quadrado').style.backgroundColor = '#11b819';
+});
+   
+document.getElementById('red').addEventListener('click', function(){
+       document.getElementById('quadrado').style.backgroundColor = '#f00'
+});
+*/
+
+/* document.getElementById('meu-cpf').addEventListener('keyup', function(){
+    const cpf = document.getElementById('meu-cpf').value;
+    document.getElementById('novo-cpf').value=cpf.slice(0,3)+"."+cpf.slice(3,6)+"."+cpf.slice(6,9)+"-"+cpf.slice(9,11);
+});
+*/
+
